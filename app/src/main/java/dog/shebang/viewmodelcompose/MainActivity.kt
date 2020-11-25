a03f1c4f0a87775d79b0f76cbc62ec1fbdfeb608
ShebangDog <e1n18088@oit.ac.jp>
2020-11-25 22:07:31 +0900

    refactor

--- app/src/main/java/dog/shebang/viewmodelcompose/MainActivity.kt
+++ app/src/main/java/dog/shebang/viewmodelcompose/MainActivity.kt
@@ -3,8 +3,6 @@ package dog.shebang.viewmodelcompose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.viewinterop.viewModel
@@ -14,15 +12,20 @@ import dog.shebang.viewmodelcompose.ui.ViewModelComposeTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ViewModelComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContent { MainApp() }
     }
+}
+
+@Composable
+fun MainApp() {
+    ViewModelComposeTheme {
+        AppContents()
+    }
+}
+
+@Composable
+fun AppContents() {
+    Greeting("Android")
 }
 
 @Composable
